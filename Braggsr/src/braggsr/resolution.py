259c8eebf241517746b0528c8daab42fe778caf6
9ix <9ix@ornl.gov>
2025-07-09 15:45:45 -0400

    Descriptions and type hinting completed 154509JUL2025

--- Braggsr/src/braggsr/resolution.py
+++ Braggsr/src/braggsr/resolution.py
@@ -28,7 +28,7 @@ from lmfit.models import GaussianModel, LorentzianModel, VoigtModel
 def NR_Resolution():
 
 #establish prefix for files
-    def nr_normalized_data (data_path):
+    def nr_normalized_data (data_path:str)->str:
         """Ingests a file, checks the path, runs through tifffile package to convert to numpy array, and then checks the data type to ensure it's compatible with the image processing regime (built for the normalized TimePix 1 file output--32 point floating bit TIF file). If the 
         image is either already a unsigned single channel 8-bit image (uint8) or a completely separate data type, the program will warn of this and then recommend the next step. 
 
@@ -53,14 +53,14 @@ def NR_Resolution():
             print ("Image does not need further conversion--skip to horizontal_image_processing function")
         else:
             print ("Image needs",img.dtype,"conversion method")
-    def data_conversion (img, alpha, beta)
+    def data_conversion (img:np.ndarray, alpha:str, beta:str)->np.ndarray:
         """Takes the image(img) identified in the nr_normalized_data function and converts it from native Float32 to uint8 for compatibility with OpenCV image processing processes. Finally, converts to a true black/white image for highest contrast prior to edge identification. For
         TPX-1 normalized images, alpha is 0, beta is 255 for conversion. 
 
         Parameters
         ----------
         img: np.ndarray (2D)
-            The image, as a callable numpy N-dimensional array (2D--position and intensity), formatted in Float32 (native data type for normalized TimePix-1 neutron radiographs)
+            The image, as a callable numpy N-dimensional array (2D), formatted in Float32 (native data type for normalized TimePix-1 neutron radiographs)
         alpha: int
             A value, between 0 and 255, that sets the floor for color-channel values (grayscale will be converted to RGB valuation with much higher granularity than 16-step grayscale)
         beta: int
@@ -69,7 +69,7 @@ def NR_Resolution():
         Returns
         -------
         bw: np.ndarray (2D)
-            A uint8-type ndarray compatible with OpenCV image processing techniques used to increase contrast and resolution. 
+            A uint8-type ndarray compatible with OpenCV image processing techniques used to increase contrast and resolution 
         """
         gray = cv.bitwise_not(img)
         cv.normalize(gray, gray, alpha, beta, norm_type=cv.NORM_MINMAX)
@@ -79,19 +79,19 @@ def NR_Resolution():
         return (bw)
     
     #creates two separate images at highest contrast to separate horizontal and vertical features
-    def horizontal_image_processing(bw):
+    def horizontal_image_processing(bw:np.ndarray)->np.ndarray:
         """Takes the black and white, uint8 type image (bw) and processes its horizontal features to increase contrast using OpenCV's erosion and dilation techniques. Introduces some smoothing (cv.blur) at the end in order to finalize contrast and reduce noise at edges prior to Canny Edge
-        detection technique being introduced to processed image.  ##do we want to do the blurring feature--increases resolution but reduces number of data points
+        detection technique being introduced to processed image.  
 
         Parameters
         ----------
         bw: np.ndarray (2D)
-            A uint8-type ndarray compatible with OpenCV image processing techniques used to increase contrast and resolution. 
+            A uint8-type ndarray compatible with OpenCV image processing techniques used to increase contrast and resolution 
 
         Returns
         -------
         horizontal: np.ndarray(2D)
-            The horizontal portions of the image, eroded, dilated, and then smoothed in order to increase contrast and resolution of the edges for follow-on Canny edge detection. 
+            The horizontal portions of the image, eroded, dilated, and then smoothed in order to increase contrast and resolution of the edges for follow-on Canny edge detection 
         """
         horizontal = np.copy(bw)
         #horizontal processing -- separates and saves horizontal features
@@ -111,19 +111,19 @@ def NR_Resolution():
         return (horizontal)
         #plt.imshow(horizontal)
 
-    def vert_image_processing(bw):
+    def vert_image_processing(bw:np.ndarray)->np.ndarray:
         """Takes the black and white, uint8 type image (bw) and processes its vertical features to increase contrast using OpenCV's erosion and dilation techniques. Introduces some smoothing (cv.blur) at the end in order to finalize contrast and reduce noise at edges prior to Canny Edge
         detection technique being introduced to processed image.  ##do we want to do the blurring feature--increases resolution but reduces number of data points?
 
         Parameters
         ----------
         bw: np.ndarray (2D)
-            A uint8-type ndarray compatible with OpenCV image processing techniques used to increase contrast and resolution. 
+            A uint8-type ndarray compatible with OpenCV image processing techniques used to increase contrast and resolution 
 
         Returns
         -------
         vertical: np.ndarray(2D)
-            The vertical portions of the image, eroded, dilated, and then smoothed in order to increase contrast and resolution of the edges for follow-on Canny edge detection. 
+            The vertical portions of the image, eroded, dilated, and then smoothed in order to increase contrast and resolution of the edges for follow-on Canny edge detection 
         """
         #vertical processing -- separates and saves vertical features
         vertical = np.copy(bw)
@@ -147,7 +147,7 @@ def NR_Resolution():
         #print (horizontal.shape)
         #print (vertical.shape)
 
-    def imagine_recombine(horizontal, alpha, vertical, beta):    
+    def imagine_recombine(horizontal:np.ndarray, alpha:float, vertical:np.ndarray, beta:float)->np.ndarray:    
         """Recombines the horizontal and vertical processed images into a single image using OpenCV weighted image stacking method (addWeighted)
 
         Parameters
@@ -157,14 +157,14 @@ def NR_Resolution():
         vertical: np.ndarray (2D)
             The vertical portion of the parent image post-processing
         alpha: float
-            Value between 0 and 1; the weighting of the horizontal portion of the image. Defaults to .5.
+            Value between 0 and 1; the weighting of the horizontal portion of the image (defaults to .5)
         beta: float
-            Value between 0 and 1-alpha; the weighting of the vertical portion of the image. Defaults to 1-alpha (.5).
+            Value between 0 and 1-alpha; the weighting of the vertical portion of the image (defaults to 1-alpha)
         
         Returns
         -------
         recombined_image: np.ndarray (2D)
-            Agglomerated 2D array representing the weighted values of the horizontal image with the vertical image weight superimposed. 
+            Agglomerated 2D array representing the weighted values of the horizontal image with the vertical image weight superimposed 
         """
         #alpha = .5
         #beta = 1-alpha
@@ -174,21 +174,21 @@ def NR_Resolution():
         return (recombined_image)
     
     #Defines the ROI and processes the image to detect and identify the edges
-    def Canny_edges (recombined_image, x1,y1,width1, height1)
+    def Canny_edges (recombined_image:np.ndarray, x1:int,y1:int,width1:int, height1:int)->np.ndarray:
         """Takes in the 2D array recombined_image and conducts the Canny edge detection across the established region of interest (ROI)
 
         Parameters
         ----------
         recombined_image: np.ndarray (2D)
-            Agglomerated 2D array representing the weighted values of the horizontal image with the vertical image weight superimposed. 
+            Agglomerated 2D array representing the weighted values of the horizontal image with the vertical image weight superimposed 
         x1: int
-            The left-most horizontal position for the desired region of interest.
+            The left-most horizontal position for the desired region of interest
         y1: int
-            The uppermost (closest to the origin--image is plotted in 3rd quadrant as abs (x,y)) vertical position for the desired region of interest. 
+            The uppermost (closest to the origin--image is plotted in 3rd quadrant as abs (x,y)) vertical position for the desired region of interest 
         width1: int
-            The width of the desired region of interest. 
+            The width of the desired region of interest 
         height1: int
-            The height for the desired region of interest. 
+            The height for the desired region of interest 
         
         Returns
         -------
@@ -201,7 +201,7 @@ def NR_Resolution():
         return (edges)
 
     #export the edge locations
-    def exp_edge_loc (edges, x1, y1,file_name_edges) #save_loc
+    def exp_edge_loc (edges:np.ndarray, x1:int, y1:int,file_name_edges:str) -> np.ndarray:
         """Takes in the 2D edges numpy array for the desired region of interest, identifies the indices of the non-zero row and column elements, combines those two lists
         (rows, columns) into a single 2D array corresponding to the coordinates of an edge pixel, converts those coordinates back to 
         parent image coordinates, and stores coordinates for plotting ROIs across parent image. 
@@ -211,17 +211,17 @@ def NR_Resolution():
         edges: np.ndarray (2D)
             All identified edges with regions of interest highlighted and stores in 2D numpy array with spans for regions of interest
         x1: int
-            The left-most horizontal position for the desired region of interest.
+            The left-most horizontal position for the desired region of interest
         y1: int
-            The uppermost (closest to the origin--image is plotted in 3rd quadrant as abs (x,y)) vertical position for the desired region of interest. 
+            The uppermost (closest to the origin--image is plotted in 3rd quadrant as abs (x,y)) vertical position for the desired region of interest 
         file_name_edges: str
-            Desired output file name. 
+            Desired output file name 
 
         Return
         ------
         edge_locations: np.ndarray (2D)
             A 2D array containing the locations of the edges in the parent image for processing of the resolution function on the parent 
-            image. 
+            image 
         """ 
         edge_locations = np.column_stack(np.where(edges != 0))
         edge_locations[:,0] += y1
@@ -229,7 +229,7 @@ def NR_Resolution():
         np.savetxt(file_name_edges, edge_locations, fmt='%d', header = "Row, Column")
         return(edge_locations)
     
-    def ROI_zones (edge_locations, x1, y1, left_range, right_range, zone_locations_fileName)
+    def ROI_zones (edge_locations:np.ndarray, x1:int, y1:int, left_range:int, right_range:int, zone_locations_fileName:str)->np.ndarray:
         """Takes in the edge locations, establishes zones of a desired height and width (default is 9 pixels wide, 1 pixel tall), and saves
         boxes as 2D array in which the y-value is steady across the box; output data represents left edge, y1 --> right edge, y1.
 
@@ -237,20 +237,20 @@ def NR_Resolution():
         ----------
         edge_locations: np.ndarray (2D)
             A 2D array containing the locations of the edges in the parent image for processing of the resolution function on the parent 
-            image.
+            image
         x1: int
-            The left-most horizontal position for the desired region of interest.
+            The left-most horizontal position for the desired region of interest
         y1: int
-            The uppermost (closest to the origin--image is plotted in 3rd quadrant as abs (x,y)) vertical position for the desired region of interest. 
+            The uppermost (closest to the origin--image is plotted in 3rd quadrant as abs (x,y)) vertical position for the desired region of interest 
         left_range: int
-            The distance from the left of x1 to the left edge of the desired zone for analysis. 
+            The distance from the left of x1 to the left edge of the desired zone for analysis 
         right_range: int
-            The distance from the right of x1 to the right edge of the zone.
+            The distance from the right of x1 to the right edge of the zone
         
         Returns
         -------
         zones: np.ndarray (2D)
-            Saves a 2D array with the zones as a series of boxes that have a single y value and x values that span from left to right lateral limits. 
+            Saves a 2D array with the zones as a series of boxes that have a single y value and x values that span from left to right lateral limits 
 
         """
         #box_width = 9
@@ -269,7 +269,7 @@ def NR_Resolution():
     
     
     #establish the class for the model comparison (picking fit model with the highest R^2 value)
-    def run_fit (zones, y1, left_lat_lim, right_lat_lim, normalized_image_file):
+    def run_fit (zones:np.ndarray, y1:int, left_lat_lim:int, right_lat_lim:int, normalized_image_file:str)->str:
         """Takes in the 2D array zones, the left and right lateral limits for the zone, and a file name. Redefines the image for analysis as the normalized, stacked image without the region of interest. Establishes
         the "Model" class and the data of interest (r-squared value of fit, full-width/half-maximum values of fit) from the LMFit Gaussian, Lorentzian, and Voigt fit models' internal parameter reports. Finally, analyzes
         the fit parameters, determines the strongest fit of the three based on the r-sqaured values, calls the FWHM value for that model (self-reported in the LMFit model report) and uses that FWHM value to determine sensor 
@@ -290,8 +290,9 @@ def NR_Resolution():
 
         Returns
         -------
-
-        Print statement detailing the data sample size, 
+        str
+            Print statements detailing the data sample size, average resolution and spatial resolution of the sensor, and the standard deviation of the data set as a 
+            measure of the strength of the data correlation 
         """
         #Saves and iterates all saved ROIs through the resolution script
         image=imageio.imread(normalized_image_file)
